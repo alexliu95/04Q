dbba762d60b7e42cc99c52bf7238e214533b93dc
alexliu95 <windylewisliu@hotmail.com>
2025-03-30 01:11:05 +0800

    bug fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
             gtag('config', ${process.env.GOOGLE_ANALYTICS});
           `}
         </Script>
-        <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_ANALYTICS}`} crossorigin="anonymous"></script>
+        <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_ADSENSE}`} crossorigin="anonymous"></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
